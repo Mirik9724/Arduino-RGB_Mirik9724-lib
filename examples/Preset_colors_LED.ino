7d7d73d991579ac033b8a4761ac51b779f81c264
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-05-19 17:49:25 +1000

    Add files via upload

---
+++ examples/Preset_colors_LED.ino
@@ -0,0 +1,34 @@
+#include <RGB_Mirik9724.h>
+
+For the library to work correctly, it is necessary to connect the RGB LED to the Analog input or digital inputs marked "~"
+
+RGB_Mirik9724(11, 10, 9);//pinR, pinG, pinB
+void setup() {
+  
+}
+
+void loop() {
+  color_m(2);//blue
+  delay(1000);//delay 1 second
+
+  color_m(3);//green
+  delay(1000);//delay 1 second
+
+  color_m(4);//yellow
+  delay(1000);//delay 1 second
+
+  color_m(5);//red
+  delay(1000);//delay 1 second
+
+  color_m(6);//white
+  delay(1000);//delay 1 second
+
+  color_m(7);//light blue
+  delay(1000);//delay 1 second
+
+  color_m(8);//purple
+  delay(1000);//delay 1 second
+
+  //if the number in color_m is not 2-8 inclusive then the LED will turn off
+
+}
---
+++ examples/RBG_LED.ino
@@ -0,0 +1,21 @@
+#include <RGB_Mirik9724.h>
+
+For the library to work correctly, it is necessary to connect the RGB LED to the Analog input or digital inputs marked "~"
+
+RGB_Mirik9724(11, 10, 9);//pinR, pinG, pinB
+void setup() {
+  
+}
+
+void loop() {
+  rgb_m(255, 255, 255);//white
+  delay(1000);//delay 1 second
+  rgb_m(255, 0, 0);//red
+  delay(1000);//delay 1 second
+  rgb_m(0, 255, 0);//green
+  delay(1000);//delay 1 second
+  rgb_m(0, 0, 255);//blue
+  delay(1000);//delay 1 second
+  rgb_m(0, 0, 0);//off of Light-emitting diode
+  delay(1000);//delay 1 second
+}
