fb5f6413a5a4b028b468cf901d51b3d7117c92f9
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-05-20 13:06:42 +1000

    Update RBG_LED.ino

--- examples/RBG_LED.ino
+++ examples/RBG_LED.ino
@@ -2,20 +2,20 @@
 
 For the library to work correctly, it is necessary to connect the RGB LED to the Analog input or digital inputs marked "~"
 
-RGB_Mirik9724 LED(11, 10, 9);//pinR, pinG, pinB
+RGB_Mirik9724 myled(11, 10, 9);//pinR, pinG, pinB
 void setup() {
   
 }
 
 void loop() {
-  rgb_m(255, 255, 255);//white
+  myled.rgb_m(255, 255, 255);//white
   delay(1000);//delay 1 second
-  rgb_m(255, 0, 0);//red
+  myled.rgb_m(255, 0, 0);//red
   delay(1000);//delay 1 second
-  rgb_m(0, 255, 0);//green
+  myled.rgb_m(0, 255, 0);//green
   delay(1000);//delay 1 second
-  rgb_m(0, 0, 255);//blue
+  myled.rgb_m(0, 0, 255);//blue
   delay(1000);//delay 1 second
-  rgb_m(0, 0, 0);//off of Light-emitting diode
+  myled.rgb_m(0, 0, 0);//off of Light-emitting diode
   delay(1000);//delay 1 second
 }
