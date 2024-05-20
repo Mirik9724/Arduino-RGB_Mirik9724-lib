7bd44c0495acfabcafbdff87564b6f3d8bb3c33a
Mirik9724 <145905797+Mirik9724@users.noreply.github.com>
2024-05-20 13:06:08 +1000

    Update Preset_colors_LED.ino

--- examples/Preset_colors_LED.ino
+++ examples/Preset_colors_LED.ino
@@ -2,31 +2,31 @@
 
 For the library to work correctly, it is necessary to connect the RGB LED to the Analog input or digital inputs marked "~"
 
-RGB_Mirik9724 LED(11, 10, 9);//pinR, pinG, pinB
+RGB_Mirik9724 myled(11, 10, 9);//pinR, pinG, pinB
 void setup() {
   
 }
 
 void loop() {
-  color_m(2);//blue
+  myled.color_m(2);//blue
   delay(1000);//delay 1 second
 
-  color_m(3);//green
+  myled.color_m(3);//green
   delay(1000);//delay 1 second
 
-  color_m(4);//yellow
+  myled.color_m(4);//yellow
   delay(1000);//delay 1 second
 
-  color_m(5);//red
+  myled.color_m(5);//red
   delay(1000);//delay 1 second
 
-  color_m(6);//white
+  myled.color_m(6);//white
   delay(1000);//delay 1 second
 
-  color_m(7);//light blue
+  myled.color_m(7);//light blue
   delay(1000);//delay 1 second
 
-  color_m(8);//purple
+  myled.color_m(8);//purple
   delay(1000);//delay 1 second
 
   //if the number in color_m is not 2-8 inclusive then the LED will turn off
